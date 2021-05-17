3a6836aa5c9d471aa930428dd0a62c7e5ef4b72f
Boris Alexandrin <50052307+balexandrin@users.noreply.github.com>
2021-05-17 23:36:39 +0300

    Delete task070204.py

--- task070204.py
+++
@@ -1,9 +0,0 @@
-class Square:
-    def __init__(self, a):
-        self.a = a
-        self.area = a ** 2
-
-class SquareFactory:
-    @staticmethod
-    def makeSquare(a):
-        return Square(a)
